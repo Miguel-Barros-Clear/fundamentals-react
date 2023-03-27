ebbc2a9473c8ff383c2e2ee2d1b131f586d1dd59
Miguel-Barros-Clear <eomiguelbarros@gmail.com>
2023-03-27 12:34:26 -0300

    feat: finish card component

--- src/app.jsx
+++ src/app.jsx
@@ -6,36 +6,41 @@ import Random from "./components/basics/random";
 import Secund from "./components/basics/secund";
 import Card from "./components/layout/card";
 
+import "./app.css";
+
 export default function App(props) {
   return (
-    <div id="app">
+    <div className="App">
       <h1>Funtamentos React</h1>
-      <Card title="#4 - Numero aleatorio">
-        <Random min={1} max={60} />
-      </Card>
 
-      <Card title="#3 - Exemplo de fragment">
-        <Fragment />
-      </Card>
+      <div className="Cards">
+        <Card title="#4 - Numero aleatorio" color="#FA6900">
+          <Random min={1} max={60} />
+        </Card>
 
-      <Card title="#2 - Segundo exemplo">
-        <Secund
-          title="Situação do aluno"
-          subTitle="Very good!"
-          aluno="Pedro"
-          nota={8.3}
-        />
-        <Secund
-          title="#2 - Situação do aluno"
-          subTitle="Very good!"
-          aluno="Maria"
-          nota={9.9}
-        />
-      </Card>
+        <Card title="#3 - Exemplo de fragment" color="#E94C6F">
+          <Fragment />
+        </Card>
 
-      <Card title="#1 - Primerio exemplo">
-        <First />
-      </Card>
+        <Card title="#2 - Segundo exemplo" color="#E8B71A">
+          <Secund
+            title="Situação do aluno"
+            subTitle="Very good!"
+            aluno="Pedro"
+            nota={8.3}
+          />
+          <Secund
+            title="#2 - Situação do aluno"
+            subTitle="Very good!"
+            aluno="Maria"
+            nota={9.9}
+          />
+        </Card>
+
+        <Card title="#1 - Primerio exemplo" color="#588C73">
+          <First />
+        </Card>
+      </div>
     </div>
   );
 }
--- src/components/layout/card.jsx
+++ src/components/layout/card.jsx
@@ -2,8 +2,13 @@ import "./card.css";
 import React from "react";
 
 export default function Card(props) {
+  const cardStyle = {
+    backgroundColor: props.color || "#F00",
+    borderColor: props.color || "#F00",
+  };
+
   return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
       <div className="Title">{props.title}</div>
       <div className="Content">{props.children}</div>
     </div>
