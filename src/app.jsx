c8567a5db1471cd8552b8bf43d5cd2a6b9d66bfa
Miguel-Barros-Clear <eomiguelbarros@gmail.com>
2023-03-27 12:23:40 -0300

    feat: creating card component

--- src/app.jsx
+++ src/app.jsx
@@ -4,27 +4,38 @@ import First from "./components/basics/first";
 import Fragment from "./components/basics/fragment";
 import Random from "./components/basics/random";
 import Secund from "./components/basics/secund";
+import Card from "./components/layout/card";
 
 export default function App(props) {
   return (
     <div id="app">
       <h1>Funtamentos React</h1>
-      <Random min={1} max={60} />
+      <Card title="#4 - Numero aleatorio">
+        <Random min={1} max={60} />
+      </Card>
 
-      <Fragment />
-      <First />
-      <Secund
-        title="Situação do aluno"
-        subTitle="Very good!"
-        aluno="Pedro"
-        nota={8.3}
-      />
-      <Secund
-        title="Situação do aluno"
-        subTitle="Very good!"
-        aluno="Maria"
-        nota={9.9}
-      />
+      <Card title="#3 - Exemplo de fragment">
+        <Fragment />
+      </Card>
+
+      <Card title="#2 - Segundo exemplo">
+        <Secund
+          title="Situação do aluno"
+          subTitle="Very good!"
+          aluno="Pedro"
+          nota={8.3}
+        />
+        <Secund
+          title="#2 - Situação do aluno"
+          subTitle="Very good!"
+          aluno="Maria"
+          nota={9.9}
+        />
+      </Card>
+
+      <Card title="#1 - Primerio exemplo">
+        <First />
+      </Card>
     </div>
   );
 }
---
+++ src/components/layout/card.jsx
@@ -0,0 +1,11 @@
+import "./card.css";
+import React from "react";
+
+export default function Card(props) {
+  return (
+    <div className="Card">
+      <div className="Title">{props.title}</div>
+      <div className="Content">{props.children}</div>
+    </div>
+  );
+}
