baaf622ffe8244ecf8f4ce9f18b81243616726a1
Miguel-Barros-Clear <eomiguelbarros@gmail.com>
2023-03-27 11:52:52 -0300

    feat: creating component with props

---
+++ src/components/basics/first.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function First() {
+  const msg = "Bem vindo!";
+
+  return (
+    <div>
+      <h2>Primeiro componente</h2>
+      <p>{msg}</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import React from 'react'
 import './index.css'
 
 import First from './components/basics/first'
+import Secund from './components/basics/secund'
 
 ReactDOM.render(
     <div>
-        <First></First>
+        <First />
+        <Secund
+            title="Situação do aluno"
+            subTitle="Very good!"
+            aluno="Pedro"
+            nota={9.3}
+        />
+        <Secund
+            title="Situação do aluno"
+            subTitle="Very good!"
+            aluno="Maria"
+            nota={9.9}
+        />
     </div>
     , document.getElementById('root'))
